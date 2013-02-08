721d2acadb545631218e434ab2a20ad98cba37fd
Schlomo Schapiro <github@schlomo.schapiro.org>
2013-02-08 15:42:54 +0100

    Return JSON or HTML depending on Accept header, use new ReadVmFile function and general refactoring to make nicer.

--- src/lml/vmdata.pl
+++ src/lml/vmdata.pl
@@ -14,36 +14,41 @@ use LML::Common;
 
 # input parameter, UUID of a VM
 my $search_uuid;
-if (param('uuid')) {
-	$search_uuid=param('uuid');
+if ( param('uuid') ) {
+    $search_uuid = param('uuid');
 } elsif (@ARGV) {
-	$search_uuid=lc($ARGV[0]);
+    $search_uuid = lc( $ARGV[0] );
 } else {
-	die("Give UUID address as query parameter 'uuid' or as command line parameter\n");
+    die("Give UUID address as query parameter 'uuid' or as command line parameter\n");
 }
 
-my $VM={};
-if (-r "$CONFIG{lml}{datadir}/vm.conf") {
-	local $/=undef;
-	open(VM_CONF,"<$CONFIG{lml}{datadir}/vm.conf") || die "Could not open $CONFIG{lml}{datadir}/vm.conf";
-	flock(VM_CONF, 1) || die;
-	binmode VM_CONF;
-	eval <VM_CONF> || die "Could not parse $CONFIG{lml}{datadir}/vm.conf";
-	close(VM_CONF);
-} elsif (! exists($VM->{$search_uuid})) {
-	print header(-status=>"200 No Data for VM $search_uuid found");
-	exit 0;
+my %VM_DATA;
+my $status = "200 OK";
+my $VM = ReadVmFile;
+if ( exists( $VM->{$search_uuid} ) ) {
+    %VM_DATA = %{ $VM->{$search_uuid} };
+
+} elsif ( %{$VM} ) {
+    $status = "200 No Data for VM $search_uuid found" ;
+    %VM_DATA = ( "NO_INFORMATION_AVAILABLE" => "SORRY" );
+
 } else {
-	print header(-status=>"200 No VM Data Found");
-	exit 0;
+    $status = "500 No VM Data Found" ;
+    %VM_DATA = ( "FATAL_ERROR" => "NO VM DATA FOUND" );
 }
 
-print header();
-my %VM_DATA = ( "NO_INFORMATION_AVAILABLE" => "SORRY" );
-%VM_DATA = %{$VM->{$search_uuid}} if (exists($VM->{$search_uuid}));
-print "<html><body><pre>\n".
-#	escapeHTML(Data::Dumper->Dump([\%VM_DATA],[qw(VM_DATA)]))."\n".
-	to_json(\%VM_DATA,{utf8 => 0, pretty => 1, allow_blessed => 1})."\n".
-	"</pre></body></html>\n";
+
+my $json_data = to_json( \%VM_DATA, { utf8 => 0, pretty => 1, allow_blessed => 1 } );
+
+if (Accept("text/json") >= 0.9) {
+    print header(-status => $status,-type=>"text/json");
+    print $json_data
+} else {
+    print header(-status => $status,-type=>"text/html");
+    print "<html><head><title>Details for $search_uuid</title></head>\n".
+        "<body><pre>\n" .
+        escapeHTML($json_data)."\n".
+        "</pre></body></html>\n";
+}
 
 1;
