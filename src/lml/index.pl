2118c9f354826c7b4e6c5179cf9614b74b6fcd1c
Schlomo Schapiro <github@schlomo.schapiro.org>
2013-02-08 12:54:37 +0100

    Refactor access to configuration data * shorter check if config data exists and is set * easier to read

--- src/lml/index.pl
+++ src/lml/index.pl
@@ -13,29 +13,27 @@ use LML::Common;
 
 # $LAB describes our internal view of the lab that lml manages
 # used mainly to react to renamed VMs or VMs with changed MAC adresses
-my $LAB={};
-if (-r "$CONFIG{lml}{datadir}/lab.conf") {
+my $LAB->{HOSTS} = {};
+my $labfile = Config("lml","datadir")."/lab.conf";
+if (-r $labfile) {
 	local $/=undef;
-	open(LAB_CONF,"<$CONFIG{lml}{datadir}/lab.conf") || die "Could not open $CONFIG{lml}{datadir}/lab.conf";
+	open(LAB_CONF,"<",$labfile) || die "Could not open $labfile for reading.\n";
 	flock(LAB_CONF, 1) || die;
 	binmode LAB_CONF;
-	eval <LAB_CONF> || die "Could not parse $CONFIG{lml}{datadir}/lab.conf";
+	eval <LAB_CONF> || die "Could not parse $labfile:\n$@\n";
 	close(LAB_CONF);
-} else {
-	# set up empty structure if our data file is missing
-	$LAB->{HOSTS} = {};
 }
-die '$LAB is empty' unless (scalar(%{$LAB}));
+die '$LAB is empty, your $labfile must be broken.\n' unless ( scalar( %{$LAB} ) );
 
-
-my $VM={};
-if (-r "$CONFIG{lml}{datadir}/vm.conf") {
-	local $/=undef;
-	open(VM_CONF,"<$CONFIG{lml}{datadir}/vm.conf") || die "Could not open $CONFIG{lml}{datadir}/vm.conf";
-	flock(VM_CONF, 1) || die;
-	binmode VM_CONF;
-	eval <VM_CONF> || die "Could not parse $CONFIG{lml}{datadir}/vm.conf";
-	close(VM_CONF);
+my $VM = {};
+my $vmfile = Config( "lml", "datadir" ) . "/vm.conf";
+if ( -r $vmfile ) {
+    local $/ = undef;
+    open( VM_CONF, "<$vmfile" ) || die "Could not open $vmfile for reading.\n";
+    flock( VM_CONF, 1 ) || die;
+    binmode VM_CONF;
+    eval <VM_CONF> || die "Could not parse $vmfile:\n$@\n";
+    close(VM_CONF);
 }
 
 print header();
@@ -65,50 +63,52 @@ print <<EOF;
 <table class="table-autostripe table-stripeclass:alternate table-autosort:0 table-autofilter" cellpadding="3" cellspacing="0">
 EOF
 
-print thead({-id=>"vmlist"},
-		Tr({-valign=>"top"},
-			th({-title=>"Click to sort",-class=>"table-sortable:alphanumeric filterable"},'Hostname'),
-			th({-title=>"Click to sort",-class=>"table-sortable:alphanumeric"},"VM Path"),
-			th({-title=>"Click to sort",-class=>"table-sortable:alphanumeric table-filterable"},"Contact User ID"),
-			th({-title=>"Click to sort",-class=>"table-sortable:date"},"Expires"),
-			)
-#		Tr(
-#			th('<input name="filter" size="8" onkeyup="Table.filter(this,this)">').
-#			th('&nbsp;').
-#			th('&nbsp;').
-#			th('&nbsp;')
-#		)
-	)."\n".
-"<tbody>\n";
+print thead(
+    { -id => "vmlist" },
+    Tr( { -valign => "top" }, th( { -title => "Click to sort", -class => "table-sortable:alphanumeric filterable" }, 'Hostname' ), th( { -title => "Click to sort", -class => "table-sortable:alphanumeric" }, "VM Path" ), th( { -title => "Click to sort", -class => "table-sortable:alphanumeric table-filterable" }, "Contact User ID" ), th( { -title => "Click to sort", -class => "table-sortable:date" }, "Expires" ), )
 
-for my $uuid (keys(%{$LAB->{HOSTS}})) {
-	my $expires = "unknown";
-	my $contact_user_id = "unknown";
-	my $display_vm_path = "<em>(no data available)</em>";
-	if (exists($VM->{$uuid})) {
-		eval { $expires=DateTime::Format::Flexible->parse_datetime($VM->{$uuid}->{CUSTOMFIELDS}->{$CONFIG{vsphere}{expires_field}} , 
-										european => ($CONFIG{vsphere}{expires_european}?1:0))->ymd(); };
-		$display_vm_path = $VM->{$uuid}->{PATH};
-		if (exists($CONFIG{GUI}{DISPLAY_FILTER_VM_PATH}) and $CONFIG{GUI}{DISPLAY_FILTER_VM_PATH}) {
-			$display_vm_path =~ s/$CONFIG{GUI}{DISPLAY_FILTER_VM_PATH}/$1/;
-		}
-		# lowercase contact user id so that SSchapiro and sschapiro will show up as the same and not as two in the drop-down box.
-		$contact_user_id = lc($VM->{$uuid}->{CUSTOMFIELDS}->{$CONFIG{vsphere}{contactuserid_field}}) if (exists($VM->{$uuid}->{CUSTOMFIELDS}->{$CONFIG{vsphere}{contactuserid_field}}));
-	}
-	print Tr({-class=>(exists($VM->{$uuid})?'vm_with_data':'vm_without_data')},
-		td[
-			a({	-href=>"vmdata.pl?uuid=$uuid",
-				-title=>"Details",
-				-onclick=>"return false;",
-				-rel=>"vmdata.pl?uuid=$uuid",
-				-class=>"tip vmhostname"
-				},
-				$LAB->{HOSTS}->{$uuid}->{HOSTNAME}
-			),
-			$display_vm_path,
-			$contact_user_id,
-			$expires,
-		])."\n";
+      #		Tr(
+      #			th('<input name="filter" size="8" onkeyup="Table.filter(this,this)">').
+      #			th('&nbsp;').
+      #			th('&nbsp;').
+      #			th('&nbsp;')
+      #		)
+) . "\n" . "<tbody>\n";
+
+for my $uuid ( keys( %{ $LAB->{HOSTS} } ) ) {
+    my $expires         = "unknown";
+    my $contact_user_id = "unknown";
+    my $display_vm_path = "<em>(no data available)</em>";
+    if ( exists( $VM->{$uuid} ) ) {
+        eval { $expires = DateTime::Format::Flexible->parse_datetime( $VM->{$uuid}->{CUSTOMFIELDS}->{ Config( "vsphere", "expires_field" ) }, european => ( Config( "vsphere", "expires_european" ) ? 1 : 0 ) )->ymd(); };
+        $display_vm_path = $VM->{$uuid}->{PATH};
+        if ( my $display_filter_vm_path = Config( "gui", "display_filter_vm_path" ) ) {
+            $display_vm_path =~ s/$display_filter_vm_path/$1/;
+        }
+
+        # lowercase contact user id so that SSchapiro and sschapiro will show up as the same and not as two in the drop-down box.
+        if ( exists( $VM->{$uuid}->{CUSTOMFIELDS}->{ Config( "vsphere", "contactuserid_field" ) } ) ) {
+            $contact_user_id = lc( $VM->{$uuid}->{CUSTOMFIELDS}->{ Config( "vsphere", "contactuserid_field" ) } );
+        }
+    }
+    print Tr(
+              { -class => ( exists( $VM->{$uuid} ) ? 'vm_with_data' : 'vm_without_data' ) },
+              td [
+                   a(
+                       {
+                          -href    => "vmdata.pl?uuid=$uuid",
+                          -title   => "Details",
+                          -onclick => "return false;",
+                          -rel     => "vmdata.pl?uuid=$uuid",
+                          -class   => "tip vmhostname"
+                       },
+                       $LAB->{HOSTS}->{$uuid}->{HOSTNAME}
+                   ),
+                   $display_vm_path,
+                   $contact_user_id,
+                   $expires,
+              ]
+    ) . "\n";
 }
 print <<EOF;
 		</tbody>
@@ -116,7 +116,7 @@ print <<EOF;
 	</div>
 EOF
 
-my $conffiles="<ol>\n\t<li>".join("</li>\n\t<li>",@CONFIGFILES)."</li>\n</ol>\n";
+my $conffiles = "<ol>\n\t<li>" . join( "</li>\n\t<li>", @CONFIGFILES ) . "</li>\n</ol>\n";
 print <<EOF;
 	<div id="tab-2">
 		<p>The config files are 
@@ -126,9 +126,9 @@ print <<EOF;
 		and this is the <strong>merged</strong> result of all config files:</p>
 		<pre>
 EOF
-$CONFIG{vsphere}{password}="***** hidden *****" if ($CONFIG{vsphere}{password});
+$CONFIG{vsphere}{password} = "***** hidden *****" if ( Config( "vsphere", "password" ) );
 my $confdump;
-open(*CONFDUMP,">",\$confdump) or die "Could not open memory file: $!";
+open( *CONFDUMP, ">", \$confdump ) or die "Could not open memory file: $!";
 tied(%CONFIG)->OutputConfigToFileHandle(*CONFDUMP);
 close(*CONFDUMP);
 print escapeHTML($confdump);
--- src/lml/lib/LML/Common.pm
+++ src/lml/lib/LML/Common.pm
@@ -3,23 +3,21 @@ package LML::Common;
 use strict;
 use Exporter;
 use vars qw(
-            @ISA
-            @EXPORT
-          );
-our @ISA         = qw(Exporter);
-our @EXPORT     = qw(%CONFIG @CONFIGFILES $isDebug Debug $LML_VERSION);
+  @ISA
+  @EXPORT
+);
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(%CONFIG @CONFIGFILES Config ReadDataFile $isDebug Debug $LML_VERSION);
 
 use FindBin;
 
-
 # Defaults for Data Dumper
 use Data::Dumper;
-$Data::Dumper::Sortkeys = 1; #Sort the keys in the output
-$Data::Dumper::Deepcopy = 1; #Enable deep copies of structures
-$Data::Dumper::Indent = 2; #Output in a reasonable style (but no array indexes)
-$Data::Dumper::Useqq  = 1;  # use double quoted strings with "\n" escapes
-$Data::Dumper::Purity = 1;  # extra code for correct perl representation
-
+$Data::Dumper::Sortkeys = 1;    #Sort the keys in the output
+$Data::Dumper::Deepcopy = 1;    #Enable deep copies of structures
+$Data::Dumper::Indent   = 2;    #Output in a reasonable style (but no array indexes)
+$Data::Dumper::Useqq    = 1;    # use double quoted strings with "\n" escapes
+$Data::Dumper::Purity   = 1;    # extra code for correct perl representation
 
 use Fcntl 'SEEK_SET';
 use File::Path;
@@ -29,60 +27,89 @@ use File::Glob ':glob';
 use IO::Handle;
 use Path::Class 'dir';
 use Cwd 'realpath';
-use DateTime::Format::Flexible;
-
+#use DateTime::Format::Flexible;
 use Config::IniFiles;
 
 use POSIX;
 
 # our version, patched by Makefile
-our $LML_VERSION="DEVELOPMENT_LML_VERSION";
+our $LML_VERSION = "DEVELOPMENT_LML_VERSION";
 
 # debugging
-our $isDebug = defined($ENV{LML_DEBUG});
+our $isDebug = defined( $ENV{LML_DEBUG} );
+
 sub Debug {
-	print STDERR "DEBUG: ".join("\nDEBUG: ",@_)."\n" if ($isDebug);
+    print STDERR "DEBUG: " . join( "\nDEBUG: ", @_ ) . "\n" if ($isDebug);
 }
 Debug("Our \@INC list looks like this:");
 Debug(@INC);
-# open main config file case-insensitive 
+
+# open main config file case-insensitive
 our %CONFIG;
 my $conf;
-unless ($ENV{HOME}) {
-	# set HOME from NSS if not set to prevent Perl error in next line
-	$ENV{HOME} = (getpwuid($>))[7];
-	Debug("Set HOME to $ENV{HOME}");
+unless ( $ENV{HOME} ) {
+    # set HOME from NSS if not set to prevent Perl error in next line
+    $ENV{HOME} = ( getpwuid($>) )[7];
+    Debug("Set HOME to $ENV{HOME}");
 }
+
 # here we rely on the fact that @INC contains our private lib dir in the first place.
-our @CONFIGFILES=map(realpath($_),<{$INC[0]/../default.conf,/etc/lml/*.conf,$ENV{HOME}/.lml-*.conf}>);
-Debug("Our config files are: ",join(" ",@CONFIGFILES));
+our @CONFIGFILES = map( realpath($_), <{$INC[0]/../default.conf,/etc/lml/*.conf,$ENV{HOME}/.lml-*.conf}> );
+Debug( "Our config files are: ", join( " ", @CONFIGFILES ) );
 foreach my $f (@CONFIGFILES) {
-	$conf = new Config::IniFiles(	-file=>$f,
-					-nocase=>1,
-					-import=>$conf
-					) or die "Could not read '$f'";
-	if ($isDebug) {
-		Debug("Read from $f:");
-		$conf->OutputConfigToFileHandle(*STDERR, 1);
-	}
+    $conf = new Config::IniFiles(
+                                  -file   => $f,
+                                  -nocase => 1,
+                                  -import => $conf
+    ) or die "Could not read '$f'";
+    if ($isDebug) {
+        Debug("Read from $f:");
+        $conf->OutputConfigToFileHandle( *STDERR, 1 );
+    }
 }
 
 if ($isDebug) {
-	Debug("Merged configuration:");
-	$conf->OutputConfigToFileHandle(*STDERR);
+    Debug("Merged configuration:");
+    $conf->OutputConfigToFileHandle(*STDERR);
 }
 
-tie %CONFIG, 'Config::IniFiles', (-import=>$conf,-nocase=>1) or die "Could not tie to config.";
+tie %CONFIG, 'Config::IniFiles', ( -import => $conf, -nocase => 1 ) or die "Could not tie to config.";
+
+sub Config($$) {
+    my $section = shift;
+    my $key = shift;
+    if (exists($CONFIG{$section}{$key})) {
+        return $CONFIG{$section}{$key};
+    } else {
+        return undef;
+    }
+}
 
 # some config checks
-die "Missing or invalid LML.DATADIR ($CONFIG{lml}{datadir}) from configuration\n" unless (-d $CONFIG{lml}{datadir});
+die "Missing or invalid LML.DATADIR (".Config("lml","datadir").") from configuration\n" unless ( -d Config("lml","datadir") );
 
 # setup vSphere environment
-die "Missing VSPHERE configuration section in configuration\n" unless ($CONFIG{vsphere});
-$ENV{VI_USERNAME}=$CONFIG{vsphere}{username} if ($CONFIG{vsphere}{username});
-$ENV{VI_PASSWORD}=$CONFIG{vsphere}{password} if ($CONFIG{vsphere}{password});
-$ENV{VI_SERVER}=$CONFIG{vsphere}{server} if ($CONFIG{vsphere}{server});
-$ENV{VI_PASSTHROUGHAUTH}=$CONFIG{vsphere}{passthroughauth} if ($CONFIG{vsphere}{passthroughauth});
-$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME}=0 if ($CONFIG{vsphere}{disablecertificatevalidation});
+die "Missing VSPHERE configuration section in configuration\n" unless ( $CONFIG{vsphere} );
+$ENV{VI_USERNAME}                  = Config("vsphere","username")        if ( Config("vsphere","username") );
+$ENV{VI_PASSWORD}                  = Config("vsphere","password")        if ( Config("vsphere","password") );
+$ENV{VI_SERVER}                    = Config("vsphere","server")          if ( Config("vsphere","server") );
+$ENV{VI_PASSTHROUGHAUTH}           = Config("vsphere","passthroughauth") if ( Config("vsphere","passthroughauth") );
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0                                 if ( Config("vsphere","disablecertificatevalidation") );
 
+# read data file
+# $1 is file in data dir
+# $2 is hashref to work on - must match the one used in the data file
+# NOTE: THIS DOES NOT YET WORK, NEED TO MAKE SURE THAT THE HASHREF IS CALLED THE SAME AS IN THE DATA FILE. OR MIGRATE TO JSON STORAGE...
+sub ReadDataFile($%) {
+    my $datafile = Config( "lml", "datadir" ) . "/" . shift;
+    my $hashref = shift;
+	if ( -r $datafile ) {
+	    local $/ = undef;
+	    open( DATAFILE, "<", $datafile ) || die "Could not open $datafile for reading.\n";
+	    flock( DATAFILE, 1 ) || die;
+	    binmode DATAFILE;
+	    eval <DATAFILE> || die "Could not parse $datafile:\n$@\n";
+	    close(DATAFILE);
+	}
+}
 1;
