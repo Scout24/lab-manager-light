a2088534197a6fbe6e3bead50833beb2c4faa878
Schlomo Schapiro <github@schlomo.schapiro.org>
2013-02-12 10:12:48 +0100

    Reformat Source Code, Use Config() instead of $CONFIG

--- src/lml/lib/LML/DHCP.pm
+++ src/lml/lib/LML/DHCP.pm
@@ -3,53 +3,55 @@ package LML::DHCP;
 use strict;
 use Exporter;
 use vars qw(
-            $VERSION
-            @ISA
-            @EXPORT
-           );
-our $VERSION     = 1.00;
-our @ISA         = qw(Exporter);
-our @EXPORT   	= qw(UpdateDHCP);
+  $VERSION
+  @ISA
+  @EXPORT
+);
+our $VERSION = 1.00;
+our @ISA     = qw(Exporter);
+our @EXPORT  = qw(UpdateDHCP);
 
 use LML::Common;
 
 sub UpdateDHCP($) {
     my ($LAB) = @_;
 
-    my @error=();
-    if (exists($CONFIG{DHCP}{hostsfile}) and $CONFIG{DHCP}{hostsfile}) {
-        my $dhcp_hosts="";
-        for my $u (keys(%{$LAB->{HOSTS}})) {
-            my $count=0;
-            if (exists($LAB->{HOSTS}->{$u}->{MACS})) {
-                for my $m (sort(@{$LAB->{HOSTS}->{$u}->{MACS}})) {
-                    $dhcp_hosts .= "host $u".($count>0?"-$count":"")." { \n";
+    my @error = ();
+    if ( my $dhcpconf = Config( "dhcp", "hostsfile" ) ) {
+        my $dhcp_hosts = "";
+        for my $u ( keys( %{ $LAB->{HOSTS} } ) ) {
+            my $count = 0;
+            if ( exists( $LAB->{HOSTS}->{$u}->{MACS} ) ) {
+                for my $m ( sort( @{ $LAB->{HOSTS}->{$u}->{MACS} } ) ) {
+                    $dhcp_hosts .= "host $u" . ( $count > 0 ? "-$count" : "" ) . " { \n";
                     $dhcp_hosts .= "\thardware ethernet $m;\n";
-                    my $hostname = $LAB->{HOSTS}->{$u}->{HOSTNAME}.($count>0?"-$count":"");	
-                    $dhcp_hosts .= "\toption host-name \"$hostname".(exists($CONFIG{dhcp}{appenddomain})?".".$CONFIG{dhcp}{appenddomain}:"")."\";\n";
+                    my $hostname = $LAB->{HOSTS}->{$u}->{HOSTNAME} . ( $count > 0 ? "-$count" : "" );
+                    $dhcp_hosts .= "\toption host-name \"$hostname" . ( Config( "dhcp", "appenddomain" ) ? "." . Config( "dhcp", "appenddomain" ) : "" ) . "\";\n";
                     # the following forces the dhcpd to update the DNS records even if the client did NOT send a hostname!!!
                     # took me full day to figure that out :-(
                     $dhcp_hosts .= "\tddns-hostname \"$hostname\";\n";
-                    $dhcp_hosts .= "\tfixed-address $LAB->{HOSTS}->{$u}->{IP};\n" if (exists($LAB->{HOSTS}->{$u}->{IP}));
-                    $dhcp_hosts .= $LAB->{HOSTS}->{$u}->{EXTRAOPTS}."\n" if (exists($LAB->{HOSTS}->{$u}->{EXTRAOPTS}));
+                    $dhcp_hosts .= "\tfixed-address $LAB->{HOSTS}->{$u}->{IP};\n" if ( exists( $LAB->{HOSTS}->{$u}->{IP} ) );
+                    $dhcp_hosts .= $LAB->{HOSTS}->{$u}->{EXTRAOPTS} . "\n" if ( exists( $LAB->{HOSTS}->{$u}->{EXTRAOPTS} ) );
                     $dhcp_hosts .= "}\n\n";
                     $count++;
                 }
             } else {
-                warn "VERY STRANGE: No MACs found for VM ".$LAB->{HOSTS}->{$u}->{HOSTNAME}." ($u)\n";
-                warn Data::Dumper->Dump([$LAB->{HOSTS}->{$u}],[qw(LAB_HOSTS_uuid)]);
-                warn "Please try to found out how this VM could PXE boot off LML without having a NIC in the managed network :-)\n"
+                warn "VERY STRANGE: No MACs found for VM " . $LAB->{HOSTS}->{$u}->{HOSTNAME} . " ($u)\n";
+                warn Data::Dumper->Dump( [ $LAB->{HOSTS}->{$u} ], [qw(LAB_HOSTS_uuid)] );
+                warn "Please try to found out how this VM could PXE boot off LML without having a NIC in the managed network :-)\n";
             }
         }
-        open(DHCP_HOSTS,">$CONFIG{DHCP}{hostsfile}") || die "Could not open '$CONFIG{DHCP}{hostsfile}' for writing";
-        flock(DHCP_HOSTS,2) || die;
+        open( DHCP_HOSTS, ">", $dhcpconf ) || die "Could not open '$dhcpconf' for writing";
+        flock( DHCP_HOSTS, 2 ) || die;
         print DHCP_HOSTS $dhcp_hosts;
         close(DHCP_HOSTS);
+
         # reload dhcp server
-        my $result = qx($CONFIG{DHCP}{TRIGGERCOMMAND} 2>&1);
-        if ($? > 0) {
-            warn "trigger command '$CONFIG{DHCP}{TRIGGERCOMMAND}' failed:\n$result";
-            push(@error,"Could not reload DHCP server, please call for help");
+        my $result = qx($CONFIG{dhcp}{triggercommand} 2>&1);
+        if ( $? > 0 ) {
+            warn "trigger command '$CONFIG{dhcp}{triggercommand}' failed:\n$result";
+            push( @error, "Could not reload DHCP server, please call for help" );
+
             # FIXME: Rollback last change or something
         }
     }
--- src/lml/lib/LML/Subversion.pm
+++ src/lml/lib/LML/Subversion.pm
@@ -3,80 +3,78 @@ package LML::Subversion;
 use strict;
 use Exporter;
 use vars qw(
-            $VERSION
-            @ISA
-            @EXPORT
-           );
-our $VERSION     = 1.00;
-our @ISA         = qw(Exporter);
-our @EXPORT   	= qw(svnCheckPath svnCopyPath svnMovePath);
-
+  $VERSION
+  @ISA
+  @EXPORT
+);
+our $VERSION = 1.00;
+our @ISA     = qw(Exporter);
+our @EXPORT  = qw(svnCheckPath svnCopyPath svnMovePath);
 
 sub svnCopyPath ($$) {
-	my ($source,$destination) = @_;
+    my ( $source, $destination ) = @_;
 
-	my $result = qx(svn copy -m "lml triggered" $source $destination 2>&1);
-	if ($? > 0) {
-		warn "'svn copy -m 'lml triggered' $source $destination' failed:\n$result";
-		return undef;
-	}
-	#warn "copied $source -> $destination";
-	return 1;
+    my $result = qx(svn copy -m "lml triggered" $source $destination 2>&1);
+    if ( $? > 0 ) {
+        warn "'svn copy -m 'lml triggered' $source $destination' failed:\n$result";
+        return undef;
+    }
+
+    #warn "copied $source -> $destination";
+    return 1;
 }
 
 sub svnMovePath ($$) {
-	my ($source,$destination) = @_;
+    my ( $source, $destination ) = @_;
 
-	my $result = qx(svn move -m "lml triggered" $source $destination 2>&1);
-	if ($? > 0) {
-		warn "'svn move -m 'lml triggered' $source $destination' failed:\n$result";
-		return undef;
-	}
-	#warn "moved $source $destination";
-	return 1;
-}					
+    my $result = qx(svn move -m "lml triggered" $source $destination 2>&1);
+    if ( $? > 0 ) {
+        warn "'svn move -m 'lml triggered' $source $destination' failed:\n$result";
+        return undef;
+    }
+
+    #warn "moved $source $destination";
+    return 1;
+}
 use SVN::Client;
 
 my $emptysub = sub {
-	my( $path, $info, $pool ) = @_;
+    my ( $path, $info, $pool ) = @_;
 };
 
 my $log_msg_callback = sub {
-	my ($msg,$tmpfile,$items,$pool) = @_;
-	$msg="lml";
+    my ( $msg, $tmpfile, $items, $pool ) = @_;
+    $msg = "lml";
 };
 
 #$ctx->log_msg($log_msg_callback);
 
 sub svnCheckPath ($) {
-	my $svnpath = shift;
-	my $ctx = new SVN::Client();
-	eval {
-		$ctx->info($svnpath, undef, "HEAD", $emptysub , 0);
-	};
-	if ($@) {
-		return undef;
-	}
-	return 1;
+    my $svnpath = shift;
+    my $ctx     = new SVN::Client();
+    eval { $ctx->info( $svnpath, undef, "HEAD", $emptysub, 0 ); };
+    if ($@) {
+        return undef;
+    }
+    return 1;
 }
 
 sub simple_prompt {
-	my $cred = shift;
-	my $realm = shift;
-	my $default_username = shift;
-	my $may_save = shift;
-	my $pool = shift;
+    my $cred             = shift;
+    my $realm            = shift;
+    my $default_username = shift;
+    my $may_save         = shift;
+    my $pool             = shift;
 
-	print "Enter authentication info for realm: $realm\n";
-	print "Username: ";
-	my $username = <>;
-	chomp($username);
-	$cred->username($username);
-	print "Password: ";
-	my $password = <>;
-	chomp($password);
-	$cred->password($password);
+    print "Enter authentication info for realm: $realm\n";
+    print "Username: ";
+    my $username = <>;
+    chomp($username);
+    $cred->username($username);
+    print "Password: ";
+    my $password = <>;
+    chomp($password);
+    $cred->password($password);
 }
-
 
 1;
