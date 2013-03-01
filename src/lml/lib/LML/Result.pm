b6c9c1f0fb02c11d6341a2f833ab74293f854960
Schlomo Schapiro <github@schlomo.schapiro.org>
2013-03-01 14:51:02 +0100

    Fix redirect bug

--- src/lml/lib/LML/Result.pm
+++ src/lml/lib/LML/Result.pm
@@ -11,12 +11,13 @@ use Carp;
 sub new {
     my ( $class, $config, $full_url ) = @_;
 
-    croak( "1st parameter to " . ( caller(0) )[3] . " must be a LML::Common::Config object" ) unless ( ref($config) eq "LML::Config" );
+    croak( "1st parameter to " . ( caller(0) )[3] . " must be a LML::Common::Config object" )
+      unless ( ref($config) eq "LML::Config" );
     my $self = {
                  config          => $config,
                  status          => "200 OK",
                  errors          => [],
-                 statusinfo        => "",
+                 statusinfo      => "",
                  redirect_target => "",
                  full_url        => $full_url
     };
@@ -66,8 +67,8 @@ sub get_errors {
 sub render {
     my ( $self, @body ) = @_;
     my $status = $self->{status};
-    if ($self->{statusinfo}) {
-        $status .= " and ".$self->{statusinfo};
+    if ( $self->{statusinfo} ) {
+        $status .= " and " . $self->{statusinfo};
     }
     if ( $self->get_errors ) {
         $status .= ", Errors: " . join( ", ", $self->get_errors );
@@ -79,7 +80,13 @@ sub render {
 
     if ( $self->{redirect_target} ) {
         my $redirect_base = $self->{full_url};
-        $redirect_base =~ s#pxelinux.cfg/.*$##;                       # strip pxelinux.cfg/*
+        if ( substr( $self->{redirect_target}, 0, 1 ) eq "/" ) {
+                                                                    # redirect to absolute url on our host
+            $redirect_base =~ s#^(.*://[^/]+).*$#$1#;              # strip everything after the host part
+        } else {
+            # redirect to relative url (probably to /boot/) on our host
+            $redirect_base =~ s#pxelinux.cfg/.*$##;                 # strip pxelinux.cfg/*
+        }
         $header_args->{-location} = $redirect_base . $self->{redirect_target};
     }
     return header($header_args) . join( "\n", @body );
