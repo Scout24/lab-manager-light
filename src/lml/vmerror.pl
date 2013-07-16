385390832f8808d76febee4052ce82018fc63ced
Christian Wernitz <christian.wernitz@onlinehome.de>
2013-07-16 16:17:01 +0200

    first Version to show Error as qrcode

---
+++ src/lml/vmerror.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# place DLLs and PMs with the required subdirectory structure into lib/ next to this script
+use FindBin;
+use lib "$FindBin::RealBin/lib";
+
+use GD::Barcode::QRcode;
+use GD::Image;
+use JSON;
+use CGI ':standard';
+
+sub display_vm_error {
+
+
+        my $data = shift;
+        my $json_data = to_json( $data, { utf8 => 0, pretty => 1, allow_blessed => 1, canonical => 1 } );
+       # return data as PNG only if we return data for a single VM, otherwise it would be too much data for the QR code.
+        my $im = new GD::Image( 640, 480 );    # always return VGA-sized image
+        my $black  = $im->colorAllocate( 0, 0, 0 );
+
+        $im->fill( 0, 0, $black );
+        # image height = (17+(Version*4)+8)*ModuleSize (learned from GD::Barcode::QRcode source)
+        $im->copy( GD::Barcode::QRcode->new( $json_data, { Ecc => 'M', Version => 23, ModuleSize => 2 } )->plot,
+                   0, 195, 0, 0, 230, 230 );
+        return $im->png;
+}
+
+my @Test = ("Fehler 1", "Fehler 2", "Fehler 3", "Fehler 4 dadadadadasdadasldfjasdladifjakdfjakjdf");
+
+my $result = display_vm_error( \@Test );
+
+print header(
+                  -status => ( $result ? 200 : 404 ),
+                  -type => "image/png",
+                  -Content_length => length($result) );
+
+print $result;
