1076473776c8ba1213e96f9921c7de67f978f1db
Florian Grundig <florian.grundig@gmx.de>
2013-07-22 15:39:07 +0200

    added network assignment logic for second interface

--- src/lml/lib/LML/VMnetworks.pm
+++ src/lml/lib/LML/VMnetworks.pm
@@ -2,6 +2,7 @@ package LML::VMnetworks;
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 sub new {
     my ( $class, $config, $host_view ) = @_;
@@ -14,9 +15,100 @@ sub new {
     return $self;
 }
 
+
+
+# Find all networks related to this vm
+# ====================================
+
+sub find_networks {
+    my ( $self, $vm_name, $force_network ) = @_;
+    my @network_labels = $self->_find_first_network($vm_name, $force_network);
+    push @network_labels, $self->_find_second_network($vm_name, @network_labels);
+    
+    my @vm_nics = $self->_create_nics_by_network_labels(@network_labels);
+    
+    return @vm_nics;
+}
+
+sub _find_first_network {
+    my ( $self, $vm_name, $force_network ) = @_;
+    my @vm_networks_labels;
+
+    # Check if we forced to a network, or if the network should be detected automatically
+    if ( defined $force_network and length $force_network ) {
+        push @vm_networks_labels, $force_network;
+    }
+    # Network should be detected automatically
+    else {
+        # Get the parameters related to network assignment logic
+        my @network_search_order = $self->{config}->get_array( "new_vm", "network_search_order" );
+        
+        # The search order controls the outer loop
+      LOOP:
+        foreach my $net_label (@network_search_order) {
+            # Use the current net_label as index for the network assignment structure
+            foreach my $rule ( $self->{config}->get_array( "network_assignment", $net_label ) ) {
+                if ( $vm_name =~ m/$rule$/x ) {
+                    push @vm_networks_labels, $net_label;
+                    # Set the loop marker to done and quit this loop
+                    last LOOP;
+                }
+            }
+        }
+    }
+    
+    return @vm_networks_labels;
+}
+
+sub _find_second_network {
+    my ($self, $vm_name, @network_labels) = @_;
+    
+    # Read out patterns which vm name should match, if the vm should get an 2nd interface
+    my @second_interface_patterns =  $self->{config}->get_array( "new_vm", "2nd_interface" );
+    # Read out the string, which should be appended to indentify the 2nd interface network label
+    my $second_interface_suffix =  $self->{config}->get( "new_vm", "2nd_interface_suffix" );
+    
+    my @second_network_labels = ();
+    
+    # Loop through each found pattern and try if the vm name is matching against one
+    foreach my $pattern (@second_interface_patterns) {
+           # If we found a pattern which is matching, the vm is allowed to get an 2nd interface
+           if ( $vm_name =~ m/^$pattern$/x ) {
+               # So take every submitted network label, put the configured 2nd_interface_suffix to the labels
+               # and create a new array with that labels
+               foreach my $network_label (@network_labels) {
+                   push @second_network_labels, $network_label . $second_interface_suffix;
+               }
+               # Our work is done here, so break the outer loop
+               last;
+           }
+    }    
+    
+    # Finally return the array with the generated 2nd interface pendants
+    return @second_network_labels;
+}
+
+sub _create_nics_by_network_labels {
+    my ($self, $network_labels) = @_;
+    my @vm_nics;
+    # After retrieving a full list of networks to be assigned, get the appropriate specs
+    # Begin with getting all networks, which the selected esx host can see
+    my $full_network_list = Vim::get_views( mo_ref_array => $self->{host_view}->network );
+
+    # Go through each network, which is assigned to the host vieww
+    foreach my $network (@$full_network_list) {
+        if ( grep { $_ eq $network->name } $network_labels ) {
+            push @vm_nics, _create_nic( network => $network );
+        }
+    }
+
+    # When we finished, return the generated network cards as an array
+    return @vm_nics;
+}
+
 # generate a spec of a networkcard
 # ================================
-sub create_nic {
+sub _create_nic {
     my %args = @_;
 
     # get the dv view of the actual network
@@ -51,51 +143,6 @@ sub create_nic {
 
     # return the generated spec
     return $nic_vm_dev_conf_spec;
-}
-
-# Find all networks related to this vm
-# ====================================
-sub find_networks {
-    my ( $self, $vm_name, $force_network ) = @_;
-    my @vm_nics;
-    my @vm_networks_labels;
-
-    # Check if we forced to a network, or if the network should be detected automatically
-    if ( defined $force_network and length $force_network ) {
-        push @vm_networks_labels, $force_network;
-    }
-    # Network should be detected automatically
-    else {
-        # Get the parameters related to network assignment logic
-        my @network_search_order = $self->{config}->get_array( "new_vm", "network_search_order" );
-
-        # The search order controls the outer loop
-      LOOP:
-        foreach my $net_label (@network_search_order) {
-            # Use the current net_label as index for the network assignment structure
-            foreach my $rule ( $self->{config}->get_array( "network_assignment", $net_label ) ) {
-                if ( $vm_name =~ m/$rule$/x ) {
-                    push @vm_networks_labels, $net_label;
-                    # Set the loop marker to done and quit this loop
-                    last LOOP;
-                }
-            }
-        }
-    }
-
-    # After retrieving a full list of networks to be assigned, get the appropriate specs
-    # Begin with getting all networks, which the selected esx host can see
-    my $full_network_list = Vim::get_views( mo_ref_array => $self->{host_view}->network );
-
-    # Go through each network, which is assigned to the host vieww
-    foreach my $network (@$full_network_list) {
-        if ( grep { $_ eq $network->name } @vm_networks_labels ) {
-            push @vm_nics, create_nic( network => $network );
-        }
-    }
-
-    # When we finished, return the generated network cards as an array
-    return @vm_nics;
 }
 
 1;
--- test/unit/11-VMnetworks.t
+++ test/unit/11-VMnetworks.t
@@ -1,12 +1,48 @@
 use strict;
-use warnings;
+no warnings 'redefine';
 
 use Test::More;
+use LML::Config;
+use Data::Dumper;
 
 BEGIN {
     use_ok "LML::VMnetworks";
 }
 
-# To be implemented
+# redefine functions with vm api access
+sub LML::VMnetworks::_create_nics_by_network_labels {
+    my ( $self, @network_labels ) = @_;
+    return @network_labels;
+}
+
+my $test_config = new LML::Config(
+    {
+       "new_vm" => {
+                     "network_search_order" => [ "DEVNIC_STATIC", "DEVNIC_DYNAMIC" ],
+                     "2nd_interface_suffix" => "_FE",
+                     "2nd_interface"        => [".*web.*"]
+       },
+       "network_assignment" => { "devnic_static" => ["^devlts.*","^devweb.*"], "devnic_dynamic" => ["^dev.*"] }
+    }
+);
+
+my $vm_networks = new_ok( "LML::VMnetworks" => [$test_config], "should create" );
+
+
+my @network_labels = $vm_networks->find_networks("otherNoMatch01");
+is_deeply( \@network_labels, [], "should return no net" );
+
+@network_labels = $vm_networks->find_networks("devlts01");
+is_deeply( \@network_labels, ["DEVNIC_STATIC"], "should return static net" );
+
+@network_labels = $vm_networks->find_networks("devxxx01");
+is_deeply( \@network_labels, ["DEVNIC_DYNAMIC"], "should return dynamic net" );
+
+@network_labels = $vm_networks->find_networks("devweb01");
+is_deeply( \@network_labels, ["DEVNIC_STATIC", "DEVNIC_STATIC_FE"], "should return static and fe network" );
+
+@network_labels = $vm_networks->find_networks("devweb01");
+is_deeply( \@network_labels, ["DEVNIC_STATIC", "DEVNIC_STATIC_FE"], "should return static and fe network" );
+
 
 done_testing();
