0eb92abbe88421bfb0289fb8735ea8a42be85ff2
Florian Grundig <florian.grundig@gmx.de>
2013-11-11 08:31:02 +0100

    fixed missing waiting image when site is opened the first time

--- src/lml/js/lml/MainController.js
+++ src/lml/js/lml/MainController.js
@@ -13,9 +13,7 @@ window.lml.MainController = function MainController($scope,$log,AjaxCallService)
   	AjaxCallService.sendAjaxCall('api/version.pl',{}, function successCallback(data){
 		$log.info("Received lml version: ",data.version);
 		$scope.version = data.version;
-		$scope.setServerRequestRunning(false);
 	}, function errorCallback(){
-	   $scope.setServerRequestRunning(false);
 	});
 
 };
