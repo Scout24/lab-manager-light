cbb9e2ec5da52cb4632ffc9d9789a706051be67b
Florian Grundig <florian.grundig@gmx.de>
2014-03-14 12:53:39 +0100

    vm-overview: removed jQuery

--- src/lml/js/lml/LabManagerLight.js
+++ src/lml/js/lml/LabManagerLight.js
@@ -25,58 +25,11 @@
     })
     .run(function run() {
 
-
-
-      // TODO: Do it in angular style
-      window.framework_height = 130;
-      window.framework_datatables_height = framework_height + 110;
-      window.min_window_height = 500;
-
-      /* customize above */
-      // TODO: Do it in angular style
-      window.myWindowHeight = function myWindowHeight() {
-        var w = $(window).height();
-        if (w < min_window_height) {
-          return (min_window_height);
-        } else {
-          return (w);
-        }
-      }
-
-      // TODO: Do it in angular style
       if (typeof String.prototype.startsWith != 'function') {
         String.prototype.startsWith = function (str) {
           return this.slice(0, str.length) == str;
         };
       }
 
-      // TODO: Do it in angular style
-      $(document).ready(function () {
-        // $("#vm_overview").append("<div id='waiting'><img src='lib/images/wait.gif' /></div>");
-
-        $('a.tip').cluetip({
-          attribute: 'href',
-          activation: 'click',
-          sticky: true,
-          closePosition: 'title',
-          arrows: true,
-          width: 500,
-          cluetipClass: 'rounded',
-          ajaxCache: false,
-          waitImage: true
-        });
-
-
-
-      });
-
-      // TODO: Do it in angular style
-      $(window).resize(function () {
-        $('.dataTables_scrollBody').css('height',
-          (myWindowHeight() - framework_datatables_height));
-        $('.tabsLite-panel').css('height',
-          (myWindowHeight() - framework_height));
-      });
-
-    })
+    });
 })();
--- src/lml/js/lml/VmOverviewController.js
+++ src/lml/js/lml/VmOverviewController.js
@@ -231,101 +231,10 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
     $scope.vms = data.vm_overview;
     filterVms('');
 
-    // INITIALIZE JQUERY DATA TABLE
-   setTimeout(function(){
-      // TODO: do this in angular style
-      $('a.tip').cluetip({
-              attribute : 'href',
-              activation : 'click',
-              sticky : true,
-              closePosition : 'title',
-              arrows : true,
-              width : 500,
-              cluetipClass : 'rounded',
-              ajaxCache : false,
-              waitImage : true
-          });
-
-    },500);
-
     $scope.setServerRequestRunning(false);
   }, function errorCallback(){
     $scope.setServerRequestRunning(false);
   });
-
-// TODO: do this in angular style
-  $.fn.destroy = function() {
-    var selectedVms = $filter("filter")($scope.filteredData, { selected : true }),
-      uuids = selectedVms.map(function(vm){ return "hosts=" + vm.uuid }).join("&")+ "&action=destroy";
-    $.ajax({
-      type: "POST",
-      beforeSend: function() {
-        /* disable the form */
-        $('#vm_action_form *').prop("disabled", "disabled");
-        $('#waiting').show();
-        /* deactivate previous error messages */
-        $('#vm_action_error').hide();
-        $('.dataTables_scrollBody').css('height',
-          (myWindowHeight() - framework_datatables_height));
-      },
-      success: function(data) {
-        /* remove the destroyed vms from our list */
-        var json = $.parseJSON(data);
-        $.each(json, function(index, value){
-          $('#' + value).remove();
-        });
-        /* reactivate the form */
-        $('#vm_action_form *').removeAttr("disabled");
-        $('#waiting').hide();
-      },
-      error: function(request, status, error) {
-        $('#vm_action_error').show();
-        $("#vm_action_error_message").text(request.responseText);
-        /* resize the result table */
-        $('.dataTables_scrollBody').css('height',
-          (myWindowHeight() - framework_datatables_height - $('#vm_action_error').outerHeight() - 8));
-        /* reactivate the form */
-        $('#vm_action_form *').removeAttr("disabled");
-        $('#waiting').hide();
-      },
-      url: "restricted/vm-control.pl?action=destroy",
-      data: uuids
-    });
-  return false;
-};
-
-
-
-// TODO: do this in angular style
-$("a.confirm").click(function(link) {
-  link.preventDefault();
-  var message = $(this).attr("rel");
-
-  $("#dialog").dialog({
-    modal: true,
-    bgiframe: true,
-    width: 450,
-    height: 215,
-    autoOpen: false,
-    title: 'Really delete?'
-  });
-
-
-  // set windows content
-  $('#dialog').html('<p>' + message + '</p>');
-
-  $("#dialog").dialog('option', 'buttons', {
-    "Delete" : function() {
-      $(this).dialog("close");
-      $.fn.destroy();
-    },
-    "Cancel" : function() {
-      $(this).dialog("close");
-    }
-  });
-  $("#dialog").dialog("open");
-});
-
 };
 
 
