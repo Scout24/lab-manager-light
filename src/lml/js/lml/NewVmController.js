654331f6237a8d4686e382570c2d818ab1ac12ce
Florian Grundig <florian.grundig@gmx.de>
2014-03-21 09:51:37 +0100

    new_vm: deleted jQuery code in new_vm controller

--- src/lml/js/lml/NewVmController.js
+++ src/lml/js/lml/NewVmController.js
@@ -60,44 +60,6 @@ window.lml.NewVmController = function NewVmController($scope, $log, AjaxCallServ
       $scope.vm_creation_failure_message = e;
     });
   };
-
-//	// TODO: do this in angular style
-//	$("#create_vm_form").submit(function(event) {
-//		/* stop form from submitting normally */
-//		event.preventDefault();
-//		var formData = $("#create_vm_form").serialize();
-//		$.ajax({
-//			type: "POST",
-//			beforeSend: function() {
-//				$('#create_vm_form *').hide();
-//				$('#vm_create_error').hide();
-//				$('#new_vm_success_title').hide();
-//				$("#info_message").text( 'Please wait while the VM will be provisioned. This can take a while ...' );
-//				$('#new_vm_progress_title').show();
-//				$('#vm_create_info').show();
-//			},
-//			success: function(data) {
-//				$('#vm_create_error').hide();
-//				$('#new_vm_progress_title').hide();
-//				$('#new_vm_success_title').show();
-//				$("#info_message").text( 'The new VM was created with the UUID ' + data );
-//				$('#vm_create_info').removeClass("info");
-//				$('#vm_create_info').addClass("success");
-//				setTimeout(function(){
-//					$('#new_vm_screenshot').attr('src', 'vmscreenshot.pl?stream=1;uuid=' + data );
-//				}, 13000);
-//			},
-//			error: function(request, status, error) {
-//				$('#vm_create_info').hide();
-//				$("#error_message").text(request.responseText);
-//				$('#vm_create_error').show();
-//			},
-//			url: "restricted/vm-create.pl",
-//			data: formData
-//		});
-//  return false;
-//});
-
 };
 
 
