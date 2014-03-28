9dc0a2a7b1dd2c59133bc71a57253497288a9d9f
Florian Grundig <florian.grundig@gmx.de>
2014-03-28 16:59:27 +0100

    vm-overview: some documentation and small refactorings

--- src/lml/js/lml/VmOverviewController.js
+++ src/lml/js/lml/VmOverviewController.js
@@ -1,16 +1,14 @@
-
 window.lml = window.lml || {};
-
-
-window.lml.VmOverviewController = function VmOverviewController($scope, $log, $location, $filter, $modal,AjaxCallService, $http) {
+window.lml.VmOverviewController = function VmOverviewController($scope, $log, $location, $filter, $modal, AjaxCallService, $http) {
   'use strict';
-  var queuedSearch,vms = [];
-  $scope.searchTerm = "";
-  $scope.detonateDisabled = false;
-  $scope.destroyDisabled  = false;
-  $scope.globals.activeTab = 'vm_overview';
-  $scope.setServerRequestRunning(true);
-  $scope.errorMsgs = '';
+  var queuedSearch, // remembers the search filter timeout to throttle down the search intervall when the user enters a search string
+    vms = [];
+
+  $scope.searchTerm = ""; // search filter
+  $scope.detonateDisabled = false; // flag whether the detonate button is disabled or not
+  $scope.destroyDisabled = false; // flag whether the destroy button is disabled or not
+  $scope.globals.activeTab = 'vm_overview'; // set the current tab so it get be marked as active when the controller is loaded via routing
+  $scope.errorMsgs = ''; // current error messages which will be displayed to the user
 
   // pagination
   $scope.itemsPerPage = 20;
@@ -18,19 +16,22 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
   $scope.currentPage = 1;
   $scope.maxSize = 5;
 
+  $scope.setServerRequestRunning(true); // will be set to false if vms were loaded
 
-  $scope.vmOverviewPicturePopup={
+  // popup for the vm screenshot (there is only one popup which will be repositioned with reloaded content regarding the selected vm)
+  $scope.vmOverviewPicturePopup = {
     style: {
       top: '0px',
       left: '0px'
     },
     display: false,
     currentVM: null,
-    close: function close(){
+    close: function close() {
       $scope.vmOverviewPicturePopup.display = false;
     }
   };
-  $scope.vmOverviewPopup={
+  // popup for the vm info (there is only one popup which will be repositioned with reloaded content regarding the selected vm)
+  $scope.vmOverviewPopup = {
     style: {
       top: '0px',
       left: '0px'
@@ -38,118 +39,66 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
     display: false,
     currentVM: null,
     content: '',
-    close: function close(){
+    close: function close() {
       $scope.vmOverviewPopup.display = false;
     }
   };
 
+  // mapping from vm json data
   $scope.tableHeaders = [
-    { name: "fullname",   title: "Hostname"},
-    { name: "vm_path",    title: "VM Path"},
+    { name: "fullname", title: "Hostname"},
+    { name: "vm_path", title: "VM Path"},
     { name: "contact_id", title: "Contact User ID"},
-    { name: "expires",    title: "Expires"},
-    { name: "esxhost",    title: "ESX Host"}
+    { name: "expires", title: "Expires"},
+    { name: "esxhost", title: "ESX Host"}
   ];
 
+  // sorting options
   $scope.sort = {
     column: '',
     descending: false
   };
 
-  $scope.getCsvValues = function(){
-    return  vms;
+  // used for the export as csv
+  $scope.getCsvValues = function () {
+    return vms; // return not only the displayed but all vms
   };
 
-  $scope.print = function(){
-    return  window.print();
+  $scope.print = function () {
+    return window.print();
   };
 
-  $scope.changeSorting = function(column) {
-    $scope.vmOverviewPopup.display = false;
-    $scope.vmOverviewPicturePopup.display = false;
-    var sort = $scope.sort;
-
-    if (sort.column === column) {
-      sort.descending = !sort.descending;
+  // when the user clicks on a table header
+  $scope.changeSorting = function (column) {
+    if ($scope.sort.column === column) {
+      $scope.sort.descending = !$scope.sort.descending;
     } else {
-      sort.column = column;
-      sort.descending = false;
+      $scope.sort.column = column;
+      $scope.sort.descending = false;
     }
-    vms.forEach(function(vm){ vm.selected = false; });
     $scope.currentPage = 1;
-    rebuildVmModel();
+    rebuildVmModel(); // for paging we need only the first (filtered) vms
   };
 
 
-  var filterVms = function(query){
-    var filteredVMs = $filter("filter")(vms, query);
+  // when the user clicks on the detonate button
+  $scope.detonate = function () {
     $scope.vmOverviewPopup.display = false;
     $scope.vmOverviewPicturePopup.display = false;
-
-    filteredVMs.forEach(function(vm){ vm.selected = false; });
-    $scope.filteredData = filteredVMs.slice(0, $scope.itemsPerPage ) // TODO anstelle der DOM-Manipulation besser Sichtbarkeit setzen (Performance-Issue)
-    $scope.totaItems = filteredVMs.length;
-    $scope.currentPage = 1;
-  };
-
-  var throttledFilterVms = function(query){
-    if (queuedSearch){
-      clearTimeout(queuedSearch);
-      queuedSearch = null;
-    }
-
-    queuedSearch = setTimeout(function(){
-      filterVms(query);
-      $scope.$apply();
-    }, 300);
-  };
-
-  var updatePage = function(){
-    rebuildVmModel();
-  };
-
-  $scope.$watch("searchTerm", throttledFilterVms);
-  $scope.$watch("currentPage", updatePage);
+    var selectedVms = $filter("filter")($scope.filteredData, { selected: true }),
+      uuids = selectedVms.map(function (vm) {
+        return "hosts=" + vm.uuid
+      }).join("&") + "&action=detonate";
 
 
-  var rebuildVmModel = function(){
-    $scope.vmOverviewPopup.display = false;
-    $scope.vmOverviewPicturePopup.display = false;
-    var filteredVMs = $filter("filter")(vms, $scope.searchTerm);
-    var sortedVMs = $filter('orderBy')(filteredVMs, $scope.sort.column, $scope.sort.descending);
-    $scope.filteredData = sortedVMs.slice($scope.itemsPerPage * ($scope.currentPage -1), $scope.itemsPerPage * ($scope.currentPage -1)+ $scope.itemsPerPage);
-    $scope.totaItems = vms.length;
-  };
-
-  var ModalInstanceCtrl = function ($scope, $modalInstance, items, action) {
-
-    $scope.items = items;
-    $scope.action = action;
-    $scope.ok = function () {
-      $modalInstance.close();
-    };
-
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-  };
-
-  $scope.detonate = function(){
-    $scope.vmOverviewPopup.display = false;
-    $scope.vmOverviewPicturePopup.display = false;
-    var selectedVms = $filter("filter")($scope.filteredData, { selected : true }),
-        uuids = selectedVms.map(function(vm){ return "hosts=" + vm.uuid }).join("&") + "&action=detonate";
-
-
-
-    if (selectedVms.length === 0 ){
+    if (selectedVms.length === 0) {
       $scope.errorMsgs = "Anzahl VMs ist 0.";
-      window.scroll(0,0);
+      window.scroll(0, 0);
       return;
     }
-    if (selectedVms.length > 3){
+    if (selectedVms.length > 3) {
       $scope.errorMsgs = "Anzahl VM > 3";
-      window.scroll(0,0);
+      window.scroll(0, 0);
       return;
     }
 
@@ -160,7 +109,7 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
         items: function () {
           return selectedVms;
         },
-        action: function(){
+        action: function () {
           return 'neu aufsetzen';
         }
       }
@@ -169,26 +118,26 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
     $scope.errorMsgs = "";
 
     modalInstance.result.then(function (selectedItem) {
-     $log.info("detonate: " + uuids);
-     $scope.$apply();
+      $log.info("detonate: " + uuids);
+      $scope.$apply();
       $scope.setServerRequestRunning(true);
-      $http.post("restricted/vm-control.pl?action=detonate", uuids, {headers: {"Content-Type" : "application/x-www-form-urlencoded"}})
-        .success(function(detonated_uuids){
-          detonated_uuids.forEach(function(detonated_uuid){
-            selectedVms.forEach(function(selectedVM){
-              if (detonated_uuid ===  selectedVM.uuid){
+      $http.post("restricted/vm-control.pl?action=detonate", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+        .success(function (detonated_uuids) {
+          detonated_uuids.forEach(function (detonated_uuid) {
+            selectedVms.forEach(function (selectedVM) {
+              if (detonated_uuid === selectedVM.uuid) {
                 selectedVM.selected = false;
-                $log.info("detonation of "+ detonated_uuid +" was successful");
+                $log.info("detonation of " + detonated_uuid + " was successful");
               }
             });
           });
-          window.scroll(0,0);
+          window.scroll(0, 0);
           $scope.setServerRequestRunning(false);
         })
-        .error(function(failure){
+        .error(function (failure) {
           $scope.setServerRequestRunning(false);
           $scope.errorMsgs = "Unkannter Fehler";
-          window.scroll(0,0);
+          window.scroll(0, 0);
         });
     }, function () {
       $log.info('Detonate modal dismissed at: ' + new Date());
@@ -196,87 +145,140 @@ window.lml.VmOverviewController = function VmOverviewController($scope, $log, $l
 
   };
 
-    $scope.destroy = function(){
-      $scope.vmOverviewPopup.display = false;
-      $scope.vmOverviewPicturePopup.display = false;
-      var selectedVms = $filter("filter")($scope.filteredData, { selected : true }),
-        uuids = selectedVms.map(function(vm){ return "hosts=" + vm.uuid }).join("&")+ "&action=destroy";
+  // when the user clicks on the destroy button
+  $scope.destroy = function () {
+    $scope.vmOverviewPopup.display = false;
+    $scope.vmOverviewPicturePopup.display = false;
+    var selectedVms = $filter("filter")($scope.filteredData, { selected: true }),
+      uuids = selectedVms.map(function (vm) {
+        return "hosts=" + vm.uuid
+      }).join("&") + "&action=destroy";
 
-      if (selectedVms.length === 0 ){
-        $scope.errorMsgs = "Anzahl VMs ist 0.";
-        window.scroll(0,0);
-        return;
-      }
-      if (selectedVms.length > 3){
-        $scope.errorMsgs = "Anzahl VM > 3";
-        window.scroll(0,0);
-        return;
-      }
-      $scope.errorMsgs = "";
+    if (selectedVms.length === 0) {
+      $scope.errorMsgs = "Anzahl VMs ist 0.";
+      window.scroll(0, 0);
+      return;
+    }
+    if (selectedVms.length > 3) {
+      $scope.errorMsgs = "Anzahl VM > 3";
+      window.scroll(0, 0);
+      return;
+    }
+    $scope.errorMsgs = "";
 
-      var modalInstance = $modal.open({
-        templateUrl: 'modalContent.html',
-        controller: ModalInstanceCtrl,
-        resolve: {
-          items: function () {
-            return selectedVms;
-          },
-          action: function(){
-            return 'physikalisch l\u00F6schen';
-          }
+    var modalInstance = $modal.open({
+      templateUrl: 'modalContent.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        items: function () {
+          return selectedVms;
+        },
+        action: function () {
+          return 'physikalisch l\u00F6schen';
         }
-      });
+      }
+    });
 
-      modalInstance.result.then(function (selectedItem) {
-        $log.info("destroy: " + uuids);
-        $scope.$apply();
-        $scope.setServerRequestRunning(true);
-        $http.post("restricted/vm-control.pl?action=destroy", uuids, {headers: {"Content-Type" : "application/x-www-form-urlencoded"}})
-          .success(function(detonated_uuids){
-            detonated_uuids.forEach(function(detonated_uuid){
-              selectedVms.forEach(function(selectedVM){
-                if (detonated_uuid ===  selectedVM.uuid){
-                  var indexOfDeletedElement = null;
-                  for (var i = 0; i < vms.length; i++){
-                    if (detonated_uuid === vms[i].uuid){
-                      indexOfDeletedElement = i;
-                      break;
-                    }
+    modalInstance.result.then(function (selectedItem) {
+      $log.info("destroy: " + uuids);
+      $scope.$apply();
+      $scope.setServerRequestRunning(true);
+      $http.post("restricted/vm-control.pl?action=destroy", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+        .success(function (detonated_uuids) {
+          detonated_uuids.forEach(function (detonated_uuid) {
+            selectedVms.forEach(function (selectedVM) {
+              if (detonated_uuid === selectedVM.uuid) {
+                var indexOfDeletedElement = null;
+                for (var i = 0; i < vms.length; i++) {
+                  if (detonated_uuid === vms[i].uuid) {
+                    indexOfDeletedElement = i;
+                    break;
                   }
-                  if (indexOfDeletedElement !== null){
-                    vms.splice(indexOfDeletedElement, 1);
-                  }
-
-                  $log.info("destroying of "+ detonated_uuid +" was successful");
                 }
-              });
+                if (indexOfDeletedElement !== null) {
+                  vms.splice(indexOfDeletedElement, 1);
+                }
+
+                $log.info("destroying of " + detonated_uuid + " was successful");
+              }
             });
-            if (detonated_uuids.length > 0){
-              rebuildVmModel();
-            }
-            window.scroll(0,0);
-            $scope.setServerRequestRunning(false);
-          })
-          .error(function(failure){
-            $scope.setServerRequestRunning(false);
-            $scope.errorMsgs = "Unkannter Fehler";
           });
-      }, function () {
-        $log.info('Destroy modal dismissed at: ' + new Date());
-      });
+          if (detonated_uuids.length > 0) {
+            rebuildVmModel();
+          }
+          window.scroll(0, 0);
+          $scope.setServerRequestRunning(false);
+        })
+        .error(function (failure) {
+          $scope.setServerRequestRunning(false);
+          $scope.errorMsgs = "Unkannter Fehler";
+        });
+    }, function () {
+      $log.info('Destroy modal dismissed at: ' + new Date());
+    });
+  };
 
+  // filter the vms regarding the current search filter
+  function filterVms (query) {
+    $scope.currentPage = 1;
+    rebuildVmModel();
+  }
 
+  // when a user enters a search filter wait for 300 ms befor sending the next query, so fast typing do not lead to a request each letter
+  function throttledFilterVms (query) {
+    if (queuedSearch) {
+      clearTimeout(queuedSearch);
+      queuedSearch = null;
+    }
+
+    queuedSearch = setTimeout(function () {
+      filterVms(query);
+      $scope.$apply();
+    }, 300);
+  }
+
+  // rebuild the displayed vm array according to the current page and filter and sorting criteria
+  function rebuildVmModel () {
+    vms.forEach(function (vm) {
+      vm.selected = false;
+    });
+    $scope.vmOverviewPopup.display = false;
+    $scope.vmOverviewPicturePopup.display = false;
+    var filteredVMs = $filter("filter")(vms, $scope.searchTerm);
+    var sortedVMs = $filter('orderBy')(filteredVMs, $scope.sort.column, $scope.sort.descending);
+    $scope.filteredData = sortedVMs.slice($scope.itemsPerPage * ($scope.currentPage - 1), $scope.itemsPerPage * ($scope.currentPage - 1) + $scope.itemsPerPage);
+    $scope.totaItems = vms.length;
+  }
+
+  // the controller for popups
+  function ModalInstanceCtrl($scope, $modalInstance, items, action) {
+
+    $scope.items = items;
+    $scope.action = action;
+    $scope.ok = function () {
+      $modalInstance.close();
     };
 
-  AjaxCallService.get('api/vm_overview.pl',function successCallback(data){
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  }
+
+
+  $scope.$watch("searchTerm", throttledFilterVms);
+  $scope.$watch("currentPage", rebuildVmModel);
+
+
+  // initial request to load all vms
+  AjaxCallService.get('api/vm_overview.pl', function successCallback(data) {
     $scope.errorMsgs = "";
-    $log.info("Received vm overview data: ",data);
+    $log.info("Received vm overview data: ", data);
     vms = data.vm_overview;
     $scope.totaItems = vms.length;
     rebuildVmModel();
 
     $scope.setServerRequestRunning(false);
-  }, function errorCallback(){
+  }, function errorCallback() {
     $scope.setServerRequestRunning(false);
   });
 };
