fd21bb59f1b0c823539fbf2465b9967f35d70050
Florian Grundig <florian.grundig@gmx.de>
2014-04-03 16:59:40 +0200

    frontend: fixed paging error when number of vms is smaller than max page items refactored VmOverviewController -> extracted VmOverviewActionsFactory

---
+++ src/lml/js/lml/VmOverviewActionsFactory.js
@@ -0,0 +1,172 @@
+angular.module('lml-app')
+  .service('VmOverviewActionsFactory', function VmOverviewActionsFactory($log, $location, $filter, $modal, AjaxCallService, $http) {
+    'use strict';
+
+    function closeAllPopups(scope) {
+      scope.vmOverviewPopup.display = false;
+      scope.vmOverviewPicturePopup.display = false;
+    }
+
+    // the controller for popups
+    function modalInstanceCtrl($scope, $modalInstance, items, action) {
+
+      $scope.items = items;
+      $scope.action = action;
+      $scope.ok = function () {
+        $modalInstance.close();
+      };
+
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
+    }
+
+    function getDetonateAction(scope, vms, rebuildVmModel) {
+
+      return function detonate() {
+
+        closeAllPopups(scope);
+        var selectedVms = $filter("filter")(scope.filteredData, { selected: true }),
+          uuids = selectedVms.map(function (vm) {
+            return "hosts=" + vm.uuid;
+          }).join("&") + "&action=detonate";
+
+
+        if (selectedVms.length === 0) {
+          scope.errorMsgs = "Anzahl VMs ist 0.";
+          window.scroll(0, 0);
+          return;
+        }
+        if (selectedVms.length > 3) {
+          scope.errorMsgs = "Anzahl VM > 3";
+          window.scroll(0, 0);
+          return;
+        }
+        scope.errorMsgs = "";
+
+        var modalInstance = $modal.open({
+          templateUrl: 'modalContent.html',
+          controller: modalInstanceCtrl,
+          resolve: {
+            items: function () {
+              return selectedVms;
+            },
+            action: function () {
+              return 'neu aufsetzen';
+            }
+          }
+        });
+
+        scope.errorMsgs = "";
+
+        modalInstance.result.then(function (selectedItem) {
+          $log.info("detonate: " + uuids);
+          scope.$apply();
+          scope.setServerRequestRunning(true);
+          $http.post("restricted/vm-control.pl?action=detonate", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+            .success(function (detonated_uuids) {
+              detonated_uuids.forEach(function (detonated_uuid) {
+                selectedVms.forEach(function (selectedVM) {
+                  if (detonated_uuid === selectedVM.uuid) {
+                    selectedVM.selected = false;
+                    $log.info("detonation of " + detonated_uuid + " was successful");
+                  }
+                });
+              });
+              window.scroll(0, 0);
+              scope.setServerRequestRunning(false);
+            })
+            .error(function (failure) {
+              scope.setServerRequestRunning(false);
+              scope.errorMsgs = "Unkannter Fehler";
+              window.scroll(0, 0);
+            });
+        }, function () {
+          $log.info('Detonate modal dismissed at: ' + new Date());
+        });
+
+      };
+    }
+
+
+// when the user clicks on the destroy button
+    function getDestroyAction(scope, vms, rebuildVmModel) {
+      return function destroy() {
+        closeAllPopups(scope);
+        var selectedVms = $filter("filter")(scope.filteredData, { selected: true }),
+          uuids = selectedVms.map(function (vm) {
+            return "hosts=" + vm.uuid;
+          }).join("&") + "&action=destroy";
+
+        if (selectedVms.length === 0) {
+          scope.errorMsgs = "Anzahl VMs ist 0.";
+          window.scroll(0, 0);
+          return;
+        }
+        if (selectedVms.length > 3) {
+          scope.errorMsgs = "Anzahl VM > 3";
+          window.scroll(0, 0);
+          return;
+        }
+        scope.errorMsgs = "";
+
+        var modalInstance = $modal.open({
+          templateUrl: 'modalContent.html',
+          controller: modalInstanceCtrl,
+          resolve: {
+            items: function () {
+              return selectedVms;
+            },
+            action: function () {
+              return 'physikalisch l\u00F6schen';
+            }
+          }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+          $log.info("destroy: " + uuids);
+          scope.$apply();
+          scope.setServerRequestRunning(true);
+          $http.post("restricted/vm-control.pl?action=destroy", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+            .success(function (detonated_uuids) {
+              detonated_uuids.forEach(function (detonated_uuid) {
+                selectedVms.forEach(function (selectedVM) {
+                  var indexOfDeletedElement = null, i;
+                  if (detonated_uuid === selectedVM.uuid) {
+                    for (i = 0; i < vms.length; i++) {
+                      if (detonated_uuid === vms[i].uuid) {
+                        indexOfDeletedElement = i;
+                        break;
+                      }
+                    }
+                    if (indexOfDeletedElement !== null) {
+                      vms.splice(indexOfDeletedElement, 1);
+                    }
+
+                    $log.info("destroying of " + detonated_uuid + " was successful");
+                  }
+                });
+              });
+              if (detonated_uuids.length > 0) {
+                rebuildVmModel();
+              }
+              window.scroll(0, 0);
+              scope.setServerRequestRunning(false);
+            })
+            .error(function (failure) {
+              scope.setServerRequestRunning(false);
+              scope.errorMsgs = "Unkannter Fehler";
+            });
+        }, function () {
+          $log.info('Destroy modal dismissed at: ' + new Date());
+        });
+      };
+    }
+
+    return {
+      getDestroyAction: getDestroyAction,
+      getDetonateAction: getDetonateAction
+    };
+  });
+
+
--- src/lml/js/lml/VmOverviewController.js
+++ src/lml/js/lml/VmOverviewController.js
@@ -1,18 +1,55 @@
+/*global angular */
 angular.module('lml-app')
-  .controller('VmOverviewController', function VmOverviewController($scope, $log, $location, $filter, $modal, AjaxCallService, $http) {
+  .controller('VmOverviewController', function VmOverviewController($scope, $log, $filter, AjaxCallService, VmOverviewActionsFactory) {
     'use strict';
     var queuedSearch, // remembers the search filter timeout to throttle down the search intervall when the user enters a search string
       vms = [];
 
+
+    function closeAllPopups(){
+      $scope.vmOverviewPopup.display = false;
+      $scope.vmOverviewPicturePopup.display = false;
+    }
+
+    // rebuild the displayed vm array according to the current page and filter and sorting criteria
+    function rebuildVmModel() {
+      vms.forEach(function (vm) {
+        vm.selected = false;
+      });
+      closeAllPopups();
+      var filteredVMs = $filter("filter")(vms, $scope.searchTerm);
+      var sortedVMs = $filter('orderBy')(filteredVMs, $scope.sort.column, $scope.sort.descending);
+      $scope.filteredData = sortedVMs.slice($scope.itemsPerPage * ($scope.currentPage - 1), $scope.itemsPerPage * ($scope.currentPage - 1) + $scope.itemsPerPage);
+      $scope.totalItems = filteredVMs.length;
+    }
+    // filter the vms regarding the current search filter
+    function filterVms(query) {
+      $scope.currentPage = 1;
+      rebuildVmModel();
+    }
+
+    // when a user enters a search filter wait for 300 ms befor sending the next query, so fast typing do not lead to a request each letter
+    function throttledFilterVms(query) {
+      if (queuedSearch) {
+        clearTimeout(queuedSearch);
+        queuedSearch = null;
+      }
+
+      queuedSearch = setTimeout(function () {
+        filterVms(query);
+        $scope.$apply();
+      }, 300);
+    }
+
+
+
     $scope.searchTerm = ""; // search filter
-    $scope.detonateDisabled = false; // flag whether the detonate button is disabled or not
-    $scope.destroyDisabled = false; // flag whether the destroy button is disabled or not
     $scope.globals.activeTab = 'vm_overview'; // set the current tab so it get be marked as active when the controller is loaded via routing
     $scope.errorMsgs = ''; // current error messages which will be displayed to the user
 
     // pagination
     $scope.itemsPerPage = 20;
-    $scope.totaItems = 0;
+    $scope.totalItems = 0;
     $scope.currentPage = 1;
     $scope.maxSize = 5;
 
@@ -81,189 +118,11 @@ angular.module('lml-app')
       rebuildVmModel(); // for paging we need only the first (filtered) vms
     };
 
-
     // when the user clicks on the detonate button
-    $scope.detonate = function () {
-      $scope.vmOverviewPopup.display = false;
-      $scope.vmOverviewPicturePopup.display = false;
-      var selectedVms = $filter("filter")($scope.filteredData, { selected: true }),
-        uuids = selectedVms.map(function (vm) {
-          return "hosts=" + vm.uuid
-        }).join("&") + "&action=detonate";
-
-
-      if (selectedVms.length === 0) {
-        $scope.errorMsgs = "Anzahl VMs ist 0.";
-        window.scroll(0, 0);
-        return;
-      }
-      if (selectedVms.length > 3) {
-        $scope.errorMsgs = "Anzahl VM > 3";
-        window.scroll(0, 0);
-        return;
-      }
-
-      var modalInstance = $modal.open({
-        templateUrl: 'modalContent.html',
-        controller: ModalInstanceCtrl,
-        resolve: {
-          items: function () {
-            return selectedVms;
-          },
-          action: function () {
-            return 'neu aufsetzen';
-          }
-        }
-      });
-
-      $scope.errorMsgs = "";
-
-      modalInstance.result.then(function (selectedItem) {
-        $log.info("detonate: " + uuids);
-        $scope.$apply();
-        $scope.setServerRequestRunning(true);
-        $http.post("restricted/vm-control.pl?action=detonate", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
-          .success(function (detonated_uuids) {
-            detonated_uuids.forEach(function (detonated_uuid) {
-              selectedVms.forEach(function (selectedVM) {
-                if (detonated_uuid === selectedVM.uuid) {
-                  selectedVM.selected = false;
-                  $log.info("detonation of " + detonated_uuid + " was successful");
-                }
-              });
-            });
-            window.scroll(0, 0);
-            $scope.setServerRequestRunning(false);
-          })
-          .error(function (failure) {
-            $scope.setServerRequestRunning(false);
-            $scope.errorMsgs = "Unkannter Fehler";
-            window.scroll(0, 0);
-          });
-      }, function () {
-        $log.info('Detonate modal dismissed at: ' + new Date());
-      });
-
-    };
+    $scope.detonate = VmOverviewActionsFactory.getDetonateAction($scope, vms, rebuildVmModel);
 
     // when the user clicks on the destroy button
-    $scope.destroy = function () {
-      $scope.vmOverviewPopup.display = false;
-      $scope.vmOverviewPicturePopup.display = false;
-      var selectedVms = $filter("filter")($scope.filteredData, { selected: true }),
-        uuids = selectedVms.map(function (vm) {
-          return "hosts=" + vm.uuid
-        }).join("&") + "&action=destroy";
-
-      if (selectedVms.length === 0) {
-        $scope.errorMsgs = "Anzahl VMs ist 0.";
-        window.scroll(0, 0);
-        return;
-      }
-      if (selectedVms.length > 3) {
-        $scope.errorMsgs = "Anzahl VM > 3";
-        window.scroll(0, 0);
-        return;
-      }
-      $scope.errorMsgs = "";
-
-      var modalInstance = $modal.open({
-        templateUrl: 'modalContent.html',
-        controller: ModalInstanceCtrl,
-        resolve: {
-          items: function () {
-            return selectedVms;
-          },
-          action: function () {
-            return 'physikalisch l\u00F6schen';
-          }
-        }
-      });
-
-      modalInstance.result.then(function (selectedItem) {
-        $log.info("destroy: " + uuids);
-        $scope.$apply();
-        $scope.setServerRequestRunning(true);
-        $http.post("restricted/vm-control.pl?action=destroy", uuids, {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
-          .success(function (detonated_uuids) {
-            detonated_uuids.forEach(function (detonated_uuid) {
-              selectedVms.forEach(function (selectedVM) {
-                if (detonated_uuid === selectedVM.uuid) {
-                  var indexOfDeletedElement = null;
-                  for (var i = 0; i < vms.length; i++) {
-                    if (detonated_uuid === vms[i].uuid) {
-                      indexOfDeletedElement = i;
-                      break;
-                    }
-                  }
-                  if (indexOfDeletedElement !== null) {
-                    vms.splice(indexOfDeletedElement, 1);
-                  }
-
-                  $log.info("destroying of " + detonated_uuid + " was successful");
-                }
-              });
-            });
-            if (detonated_uuids.length > 0) {
-              rebuildVmModel();
-            }
-            window.scroll(0, 0);
-            $scope.setServerRequestRunning(false);
-          })
-          .error(function (failure) {
-            $scope.setServerRequestRunning(false);
-            $scope.errorMsgs = "Unkannter Fehler";
-          });
-      }, function () {
-        $log.info('Destroy modal dismissed at: ' + new Date());
-      });
-    };
-
-    // filter the vms regarding the current search filter
-    function filterVms(query) {
-      $scope.currentPage = 1;
-      rebuildVmModel();
-    }
-
-    // when a user enters a search filter wait for 300 ms befor sending the next query, so fast typing do not lead to a request each letter
-    function throttledFilterVms(query) {
-      if (queuedSearch) {
-        clearTimeout(queuedSearch);
-        queuedSearch = null;
-      }
-
-      queuedSearch = setTimeout(function () {
-        filterVms(query);
-        $scope.$apply();
-      }, 300);
-    }
-
-    // rebuild the displayed vm array according to the current page and filter and sorting criteria
-    function rebuildVmModel() {
-      vms.forEach(function (vm) {
-        vm.selected = false;
-      });
-      $scope.vmOverviewPopup.display = false;
-      $scope.vmOverviewPicturePopup.display = false;
-      var filteredVMs = $filter("filter")(vms, $scope.searchTerm);
-      var sortedVMs = $filter('orderBy')(filteredVMs, $scope.sort.column, $scope.sort.descending);
-      $scope.filteredData = sortedVMs.slice($scope.itemsPerPage * ($scope.currentPage - 1), $scope.itemsPerPage * ($scope.currentPage - 1) + $scope.itemsPerPage);
-      $scope.totaItems = vms.length;
-    }
-
-    // the controller for popups
-    function ModalInstanceCtrl($scope, $modalInstance, items, action) {
-
-      $scope.items = items;
-      $scope.action = action;
-      $scope.ok = function () {
-        $modalInstance.close();
-      };
-
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    }
+    $scope.destroy = VmOverviewActionsFactory.getDestroyAction($scope, vms, rebuildVmModel);
 
 
     $scope.$watch("searchTerm", throttledFilterVms);
@@ -275,7 +134,7 @@ angular.module('lml-app')
       $scope.errorMsgs = "";
       $log.info("Received vm overview data: ", data);
       vms = data.vm_overview;
-      $scope.totaItems = vms.length;
+      $scope.totalItems = vms.length;
       rebuildVmModel();
 
       $scope.setServerRequestRunning(false);
