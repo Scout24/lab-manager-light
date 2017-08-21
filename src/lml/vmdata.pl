367f7a402b1d45dc239bb8e50c77b6b28df456ba
Ulrich Kautz <ulrich.kautz@scout24.com>
2017-08-21 17:20:00 +0200

    #SEC-1613: Also validate UUID when provided via path_info instead of CGI parameter

--- src/lml/vmdata.pl
+++ src/lml/vmdata.pl
@@ -109,8 +109,12 @@ unless (caller) {
     } elsif ( path_info() ) {
         # or from path_info
         ( $search_uuid, $suffix ) = path_info() =~ m#/([^.]+)\.?(.*)#;
-        $suffix = "" unless (defined $suffix);
-        $search_uuid = "" unless (defined $search_uuid);
+        $suffix = ""
+            unless (defined $suffix);
+        $search_uuid = validate_with($search_uuid, $VALIDATE_UUID)
+            if defined $search_uuid;
+        $search_uuid = ""
+            unless (defined $search_uuid);
         $suffix = lc($suffix);
     } else {
         # or default to nothing
